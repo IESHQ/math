a6d1121dcedb3b8ceaa6b66f6aef9f5bdfad4a98
netplwiz <cyberry12@naver.com>
2023-01-07 14:51:21 +0900

    Added convenience

--- calc.py
+++ calc.py
@@ -13,6 +13,47 @@ def addition(cond):
 def read(target):
     return open(target).read().split('\n')
 
+def translate(z, object):
+    '''
+    translate
+    ---
+    core.translate
+    '''
+    while z < len(object):
+
+        i = 0
+        q = 0
+
+        try:
+            while q < len(object[z]):
+                i = 0
+
+                try:
+                    place = [n for n in range(len(object[z])) if object[z][n:n + 2] == ')(']
+                    object[z] = f'{object[z][:place[q] + 1]}*{object[z][place[q] + 1:]}'
+                except IndexError:
+                    pass
+
+                while i < 11:
+                    try:
+                        place = [n for n in range(len(object[z])) if object[z][n:n + 2] == f'{i}(']
+                        object[z] = f'{object[z][:place[q] + 1]}*{object[z][place[q] + 1:]}'
+
+                        place = [n for n in range(len(object[z])) if object[z][n:n + 2] == f'){i}']
+                        object[z] = f'{object[z][:place[q] + 1]}*{object[z][place[q] + 1:]}'
+
+                        print(object)
+                    except IndexError:
+                        pass
+
+                    i += 1
+                q += 1
+                z += 1
+
+        except IndexError:
+            pass
+
+
 out = []
 def output(result):
     out.append(result)
@@ -26,10 +67,9 @@ def compile(target):
     '''
 
     object = read(target)
-
-
     z = 0
 
+    translate(z, object)
     while z < len(object):
         
         if 'sin' in object:
